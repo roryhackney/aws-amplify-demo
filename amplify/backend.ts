09473c7f3a41d60e6c9e596441025ad806fb0aef
Rory Hackney <roryhackney@gmail.com>
2025-07-10 11:03:39 -0700

    rename folder

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,11 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { storage } from "./storage/resource";
+import {testFunction} from "./testFunction/resource.js";
 
 defineBackend({
   auth,
   data,
-  storage
+  storage,
+  testFunction
 });
