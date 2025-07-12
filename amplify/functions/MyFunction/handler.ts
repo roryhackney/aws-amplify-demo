a69e0ed256535d492b94e79fa64b7732b39f29c8
Rory Hackney <roryhackney@gmail.com>
2025-07-11 22:33:07 -0700

    add nonexistent secret

--- amplify/functions/MyFunction/handler.ts
+++ amplify/functions/MyFunction/handler.ts
@@ -1,8 +1,9 @@
 import type {Schema} from "../../data/resource";
+import {secret} from "@aws-amplify/backend";
 
 //must be named handler
 export const handler: Schema["MyFunction"]["functionHandler"] = async (event) => {
     const {name} = event.arguments;
-    console.log(`Hello ${name}!`);
-    return "Hello Avocado!";
+    const sec = secret("SECRET_THAT_DOES_NOT_EXIST");
+    return `Hello ${name}! Your secret is ${sec}`;
 }
