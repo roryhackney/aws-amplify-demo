6aadf5b18115225cd934c237fa02b8d00cc910e6
Rory Hackney <roryhackney@gmail.com>
2025-07-11 22:41:36 -0700

    remove secret

--- amplify/functions/MyFunction/handler.ts
+++ amplify/functions/MyFunction/handler.ts
@@ -4,6 +4,7 @@ import {secret} from "@aws-amplify/backend";
 //must be named handler
 export const handler: Schema["MyFunction"]["functionHandler"] = async (event) => {
     const {name} = event.arguments;
-    const sec = secret("SECRET_THAT_DOES_NOT_EXIST");
-    return `Hello ${name}! Your secret is ${sec}`;
+    // const sec = secret("SECRET_THAT_DOES_NOT_EXIST");
+    // name += sec;
+    return `Hello! Your secret is...? ${name}!`;
 }
